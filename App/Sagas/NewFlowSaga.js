b1365b2a8a12633e6d5d53a428b9c65598eb529e
Mohamed Mamdouh <meldakhakhni.gts@emaar.ae>
2020-09-08 21:21:36 +0400

    fix: remove non exist import

--- App/Sagas/NewFlowSaga.js
+++ App/Sagas/NewFlowSaga.js
@@ -1,7 +1,6 @@
 import {call, put} from 'redux-saga/effects';
 import {path} from 'ramda';
 import NewflowActions from '../Redux/NewFlowRedux';
-import NavigationService from '../Services/NavigationService';
 
 export function* createNewFlow(api, action) {
   const {
